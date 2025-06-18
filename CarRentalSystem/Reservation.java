06f4bac92199a12211992a26716ae738cbedf344
HarshvardhanR <rharsh989878@gmail.com>
2025-06-18 01:41:41 -0400

    added some more funtionalities

--- CarRentalSystem/Reservation.java
+++ CarRentalSystem/Reservation.java
@@ -1,5 +1,29 @@
 package CarRentalSystem;
-
+import java.util.Calendar;
+import java.util.Date;
 public class Reservation {
-    
+    Vehicle v;
+    Date from;
+    Date to;
+    RentalStore rentalStore;
+    User user;
+
+    public Reservation(Vehicle v, RentalStore rentalStore, User user, int days){
+        this.v = v;
+        this.from = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(this.from);
+        cal.add(Calendar.DATE, days);
+        this.to = cal.getTime();
+        this.rentalStore = rentalStore;
+        this.user = user;
+    }
+
+    public Date getFrom(){
+        return from;
+    }
+
+    public Date getTo(){
+        return to;
+    }
 }
---
+++ CarRentalSystem/Vehicle.java
@@ -0,0 +1,38 @@
+package CarRentalSystem;
+
+public class Vehicle {
+    protected VehicleType vehicleType;
+    protected int noOfSeats;
+    protected int kmDriven;
+    protected String model;
+    protected String licensePlate;
+    protected VehicleState vehicleState;
+
+    public int getNoOfSeats(){
+        return noOfSeats;
+    }
+
+    public int getKmDriven(){
+        return kmDriven;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setVehicleState(VehicleState vehicleState){
+        this.vehicleState = vehicleState;
+    }
+
+}
+
+class Car extends Vehicle{
+    public Car(int noOfSeats, int kmDriven, String model, String licensePlate){
+        this.vehicleType = VehicleType.CAR;
+        this.noOfSeats = noOfSeats;
+        this.kmDriven = kmDriven;
+        this.model = model;
+        this.licensePlate = licensePlate;
+        this.vehicleState = VehicleState.AVAILABLE;
+    }
+}
