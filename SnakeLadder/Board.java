1836179fd9dcffbaa2303891f32941ff73b5ef6b
HarshvardhanR <rharsh989878@gmail.com>
2025-06-20 14:58:59 -0400

    added code

--- SnakeLadder/Board.java
+++ SnakeLadder/Board.java
@@ -1,5 +1,37 @@
 package SnakeLadder;
 
+import java.util.Random;
 public class Board {
-    
+    Cell [][] cells;
+
+    public Board(int size, int numberOfSnakes, int numberOfLadders){
+        initializeBoard(size);
+        addSnakesAndLadders(numberOfSnakes,numberOfLadders, cells);
+    }
+
+    public void initializeBoard(int size){
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                cells[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void addSnakesAndLadders(int numberOfSnakes, int numberOfLadders, Cell[][] cells){
+        Random rand = new Random();
+        while(numberOfSnakes>0){
+            int start = rand.nextInt(96 - 15 + 1) + 15;
+            int end = rand.nextInt(96 - 15 + 1) + 15;
+            if(start>=end) continue;
+            Cell cell = getCell(start);
+            if(cell.getProp()!=null) continue;
+            Prop snake = new Prop(start, end);
+        }
+    }
+
+    public Cell getCell(int start){
+        int row = start/cells.length;
+        int col = start % cells.length;
+        return cells[row][col];
+    }
 }
--- SnakeLadder/Cell.java
+++ SnakeLadder/Cell.java
@@ -2,4 +2,16 @@ package SnakeLadder;
 
 public class Cell {
     Prop prop;
+
+    public Cell(){
+        prop = null;
+    }
+
+    public Prop getProp(){
+        return prop;
+    }
+
+    public void setProp(Prop prop){
+        this.prop = prop;
+    }
 }
